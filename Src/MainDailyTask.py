eff691c332fd8814eb0b7e1a8bb89b1ccd28e103
shenmishajing <shenmishajing@Gmail.com>
2020-07-02 17:50:36 +0800

    rm always watch video feature

--- Src/MainDailyTask.py
+++ Src/MainDailyTask.py
@@ -28,9 +28,9 @@ class MainDailyTask:
             if config["MainDailyTask"]["ROOM_ID"] == "":
                 Log.warning("主站任务模块up主号未配置,已停止...")
             else:
-                await self.watch()
                 await self.coin()
                 await self.share()
+                await self.watch()
             await asyncio.sleep(std235959ptm())
 
     async def watch(self):
@@ -38,51 +38,45 @@ class MainDailyTask:
         MainDailyTask_Watch = int(config["MainDailyTask"]["Watch"])
         if MainDailyTask_Watch == 0:
             return
-        elif MainDailyTask_Watch == -1:
-            while time.time() < std235959():
-                var = var + 1
-                Log.info("本次观看视频为第 %s 次" % (var))
-                Room_Id = random.choice(config["MainDailyTask"]["ROOM_ID"].split(","))
-                Log.info("本次观看选择UP的ID为 %s" % (Room_Id))
-                url = "https://api.bilibili.com/x/space/arc/search?ps=100&pn=1&mid=" + str(Room_Id)
-                data = await AsyncioCurl().request_json("GET", url)
-                need_vilst = random.choice(data["data"]["list"]["vlist"])
-
-                Log.info("本次观看的视频信息如下")
-                Log.info("标题  %s" % (need_vilst["title"]))
-                Log.info("作者  %s" % (need_vilst["author"]))
-                # Log.info("简介  %s" % (need_vilst["description"]))
-                Log.info("视频BV号  %s" % (need_vilst["bvid"]))
-                Log.info("视频AV号  %s" % (need_vilst["aid"]))
-
-                # 获取视频分P
-                url = "https://api.bilibili.com/x/player/pagelist?bvid=" + need_vilst["bvid"]
-
-                data = await AsyncioCurl().request_json("GET", url)
-
-                video_cid = data["data"][0]["cid"]
-
-                url = "https://api.bilibili.com/x/click-interface/web/heartbeat"
-
-                payload = {
-                    "aid": need_vilst["aid"],
-                    "cid": video_cid,
-                    "bvid": need_vilst["bvid"],
-                    "mid": Room_Id,
-                    "csrf": account["Token"]["CSRF"],
-                    "played_time": 0,
-                    "realtime": 0,
-                    "start_ts": int(time.time()),
-                    "type": 3,
-                    "dt": 2,
-                    "play_type": 1}
-
-                data = await AsyncioCurl().request_json("POST", url, headers = config["pcheaders"], data = payload)
-
-                if (data["code"] == 0):
-                    Log.info('视频观看成功')
-                else:
-                    Log.error("签到错误 %s" % (data["message"]))
+        # elif MainDailyTask_Watch == -1:
+        #     while time.time() < std235959():
+        #         var = var + 1
+        #         Log.info("本次观看视频为第 %s 次" % (var))
+        #         Room_Id = random.choice(config["MainDailyTask"]["ROOM_ID"].split(","))
+        #         Log.info("本次观看选择UP的ID为 %s" % (Room_Id))
+        #         url = "https://api.bilibili.com/x/space/arc/search?ps=100&pn=1&mid=" + str(Room_Id)
+        #         data = await AsyncioCurl().request_json("GET", url)
+        #         need_vilst = random.choice(data["data"]["list"]["vlist"])
+        #
+        #         Log.info("本次观看的视频信息如下")
+        #         Log.info("标题  %s" % (need_vilst["title"]))
+        #         Log.info("作者  %s" % (need_vilst["author"]))
+        #         # Log.info("简介  %s" % (need_vilst["description"]))
+        #         Log.info("视频BV号  %s" % (need_vilst["bvid"]))
+        #         Log.info("视频AV号  %s" % (need_vilst["aid"]))
+        #
+        #         # 获取视频分P
+        #         url = "https://api.bilibili.com/x/player/pagelist?bvid=" + need_vilst["bvid"]
+        #
+        #         data = await AsyncioCurl().request_json("GET", url)
+        #
+        #         video_cid = data["data"][0]["cid"]
+        #
+        #         url = "https://api.bilibili.com//x/player/playurl"
+        #
+        #         payload = {
+        #             "aid": need_vilst["aid"],
+        #             "cid": video_cid,
+        #             "qn": 80,
+        #             "otype": "json"
+        #         }
+        #
+        #         data = await AsyncioCurl().request_json("POST", url, headers = config["pcheaders"], data = payload)
+        #
+        #         if (data["code"] == 0):
+        #             Log.info('视频观看成功')
+        #         else:
+        #             Log.error("签到错误 %s" % (data["message"]))
         else:
             need_watch = MainDailyTask_Watch
         if need_watch > 0:
